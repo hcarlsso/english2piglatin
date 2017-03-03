5be09f959c0538e764e32d3e706500750dffcfd1
WASP_VB <vidits@kth.se>
2017-03-03 23:11:36 +0100

    Code to handle paragraphs and improved conversion

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from src import piglatin
 
 if __name__ == '__main__':
     '''Take a word from command line'''
-    english_word = raw_input('Please enter a single English word:')
+    englishway = raw_input('Englishway exttay: ')
     
-    piglatin_word = piglatin.from_english_word(english_word)
-    print 'The piglatin for', english_word, 'is', piglatin_word
+    igpay = piglatin.english_text_to_piglatin(englishway)
+    print 'English: ', englishway
+    print 'Piglatin:', igpay
 
--- src/piglatin.py
+++ src/piglatin.py
@@ -1,20 +1,59 @@
 #! /usr/bin/python2.7
 
-'''Takes a single word as input and returns its 
-   piglatin equivalent'''
-def from_english_word(word):
-    if is_consonant(word[0]): # First letter is a consonant
-        if is_consonant(word[1]): # Second letter is a consonant too
-            return word[2:-1] + word[-1] + word[0:2] + 'ay'
-        else:
-            return word[1:-1] + word[-1] + word[0] + 'ay'
+'''Return the piglatin equivalent of a single paragraph
+'''
+def english_text_to_piglatin(paragraph):
+    paras = paragraph.split('\n')
+    return '\n'.join([english_paragraph_to_piglatin(para) for para in paras])
+
+'''Return the piglatin equivalent of a single paragraph
+'''
+def english_paragraph_to_piglatin(paragraph):
+    sentences = paragraph.split('.')
+    return '.'.join([english_sentence_to_piglatin(sentence) for sentence in sentences])
+
+'''Return the piglatin equivalent of a single sentence
+'''
+def english_sentence_to_piglatin(line):
+    words = line.split(' ')
+    return ' '.join([english_word_to_piglatin(word) for word in words])
+
+'''Return the piglatin equivalent of a single word
+'''
+def english_word_to_piglatin(word):
+    # Check for empty strings
+    if len(word) == 0:
+        return ''
+
+    # Check that the word starts with a letter
+    if not word[0].isalpha():
+        raise ValueError, 'Unexpected character ' + word[0] + '. Aborting'
+    
+    # Construct suffixes based on word capitalization
+    vowel_suffix = 'way'
+    consonant_suffix = 'ay'
+
+    if _is_consonant(word[0]): # First letter is a consonant
+        return _consonant_word_to_piglatin(word, consonant_suffix)
     else: # First letter is a vowel
         return word + 'way'
 
-'''Returns True if the letter is consonant, otherwise
+'''Return piglatin equialent of a word beginning with a
+   cosonant cluster'''
+def _consonant_word_to_piglatin(word, suffix):
+    def _rearrange_letters(word, cluster = ''):
+        if len(word) == 0: # Needed when word contains only consonants
+            return cluster
+        elif not word[0].isalpha(): 
+            raise ValueError, 'Unexpected character ' + word[0] + '. Aborting'
+        elif _is_consonant(word[0]) and not word[0] == 'y': # Add to consonant cluster and keep checking
+            return _rearrange_letters(word[1:], cluster + word[0])
+        else: # reached a vowel
+            return word + cluster
+
+    return _rearrange_letters(word) + suffix # Add suffix to rearranged letters
+
+'''Returns True if the letter is a consonant, otherwise
    return False'''
-def is_consonant(letter):
-    if letter not in ['a', 'e', 'i', 'o', 'u']:
-        return True
-    else:
-        return False
+def _is_consonant(letter):
+    return letter.isalpha() and letter.lower() not in ['a', 'e', 'i', 'o', 'u']
