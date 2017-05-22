b44bcdc38b9defb20add5100133b30ab9d46cb23
Hakan Carlsson <hakcar@kth.se>
2017-05-22 17:50:22 +0200

    Implement file reading
    Fixes #3

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 #! /usr/bin/python2.7
 
 from src import piglatin
+import argparse
 
-if __name__ == '__main__':
+def regular_input():
     '''Take a word from command line'''
-    englishway = raw_input('Englishway exttay: ')
-    
+    return raw_input('Input English: ')
+
+def file_input(myfile):
+
+    with open(myfile, 'r') as f:
+        return f.read()
+
+def doStuff(englishway):
+
     igpay = piglatin.english_text_to_piglatin(englishway)
     print 'English: ', englishway
     print 'Piglatin:', igpay
 
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Translate some English.')
+    parser.add_argument(
+        '--file',
+        default = False,
+        type = str,
+        help = 'Read text from file'
+    )
+
+    args = parser.parse_args()
+
+
+    if args.file:
+        text = file_input(args.file)
+    else:
+        text = regular_input()
+
+    doStuff(text)
