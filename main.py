4551b35cc8645250fd27d69652bf7092a8dd33f3
Hakan Carlsson <hakcar@kth.se>
2017-05-24 16:01:11 +0200

    Update main with more comments.

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 #! /usr/bin/python2.7
+# coding: utf-8
 
 from src import piglatin
 import argparse
 
 def regular_input():
-    '''Take a word from command line'''
+    '''Take text from command line, STDIN
+    Input: NA
+    Return: string from command line.'''
     return raw_input('Input English: ')
 
 def file_input(myfile):
+    '''Read text from file.
+    Input myfile: path to file with text.
+    Return: string found in myfile.'''
 
     with open(myfile, 'r') as f:
         return f.read()
 
 def doStuff(englishway):
+    '''Main function to call with string. Does the pigtranslation.
+    Input englishway: string with english text to translate.
+    Return NA.'''
 
     igpay = piglatin.english_text_to_piglatin(englishway)
     print 'English: ', englishway
@@ -20,6 +29,7 @@ def doStuff(englishway):
 
 if __name__ == '__main__':
 
+    # Parse the command line arguments.
     parser = argparse.ArgumentParser(description='Translate some English.')
     parser.add_argument(
         '--file',
@@ -30,7 +40,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-
+    # Choose if read from command line or from file.
     if args.file:
         text = file_input(args.file)
     else:
